39145e6df21c7591c1dbb559618caaefa9cc17b4
Rene-home-pc <15114388+RenV123@users.noreply.github.com>
2021-05-13 22:06:58 +0200

    Change server to show client pages.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect Database
@@ -18,6 +18,17 @@ app.get('/', (request, response) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  //Anything that's not the api endpoints should return our react app index.html page.
+  app.get('*', (request, response) =>
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
